e5c46f198811c8efdfee80dbc68b77c8f6827cde
uwanitheekshani <uwanitheekshani@gmail.com>
2023-07-09 11:17:18 +0530

    create component using angular cli command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
